ff49e445cf8266800e666408472c3167b6900540
vasilich <vasilich@grafin.kiev.ua>
2020-07-02 11:36:23 +0300

    animated padding for smoother slide when keyboard opens

--- lib/dropdown_search.dart
+++ lib/dropdown_search.dart
@@ -344,7 +344,8 @@ class _DropdownSearchState<T> extends State<DropdownSearch<T>> {
         context: context,
         builder: (context) {
           return SingleChildScrollView(
-            child: Padding(
+            child: AnimatedPadding(
+              duration: Duration(milliseconds: 300),
               padding: EdgeInsets.only(
                 bottom: MediaQuery.of(context).viewInsets.bottom,
               ),
