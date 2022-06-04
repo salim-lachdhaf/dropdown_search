aeac6986cc04191a0b2f5c53c79ee4949c55f873
Salim Lachdhaf <slachdhaf@idsystemes.com>
2022-06-04 23:00:10 +0200

    v5.0.2

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,6 +30,19 @@ class _MyHomePageState extends State<MyHomePage> {
   final _popupBuilderKey = GlobalKey<DropdownSearchState<String>>();
   final _popupCustomValidationKey = GlobalKey<DropdownSearchState<int>>();
   final _userEditTextController = TextEditingController(text: 'Mrs');
+  final myKey = GlobalKey<DropdownSearchState<MultiLevelString>>();
+  final List<MultiLevelString> myItems = [
+    MultiLevelString(level1: "1"),
+    MultiLevelString(level1: "2"),
+    MultiLevelString(
+      level1: "3",
+      subLevel: [
+        MultiLevelString(level1: "sub3-1"),
+        MultiLevelString(level1: "sub3-2"),
+      ],
+    ),
+    MultiLevelString(level1: "4")
+  ];
   bool? _popupBuilderSelection = false;
 
   @override
@@ -530,6 +543,65 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
                 child: Text('open popup'),
               ),
+
+              ///************************[multiLevel items example]********************************///
+              Padding(padding: EdgeInsets.all(8)),
+              Text("[multiLevel items example]"),
+              Divider(),
+              DropdownSearch<MultiLevelString>(
+                key: myKey,
+                items: myItems,
+                compareFn: (i1, i2) => i1.level1 == i2.level1,
+                popupProps: PopupProps.menu(
+                  showSelectedItems: true,
+                  interceptCallBacks: true, //important line
+                  itemBuilder: (ctx, item, isSelected) {
+                    return ListTile(
+                      selected: isSelected,
+                      title: Text(item.level1),
+                      trailing: item.subLevel.isEmpty
+                          ? null
+                          : (item.isExpanded
+                              ? IconButton(
+                                  icon: Icon(Icons.arrow_drop_down),
+                                  onPressed: () {
+                                    item.isExpanded = !item.isExpanded;
+                                    myKey.currentState?.updatePopupState();
+                                  },
+                                )
+                              : IconButton(
+                                  icon: Icon(Icons.arrow_right),
+                                  onPressed: () {
+                                    item.isExpanded = !item.isExpanded;
+                                    myKey.currentState?.updatePopupState();
+                                  },
+                                )),
+                      subtitle: item.subLevel.isNotEmpty && item.isExpanded
+                          ? Container(
+                              height: item.subLevel.length * 50,
+                              child: ListView(
+                                children: item.subLevel
+                                    .map(
+                                      (e) => ListTile(
+                                        selected: myKey.currentState
+                                                ?.getSelectedItem?.level1 ==
+                                            e.level1,
+                                        title: Text(e.level1),
+                                        onTap: () {
+                                          myKey.currentState
+                                              ?.popupValidate([e]);
+                                        },
+                                      ),
+                                    )
+                                    .toList(),
+                              ),
+                            )
+                          : null,
+                      onTap: () => myKey.currentState?.popupValidate([item]),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
@@ -673,4 +745,30 @@ class CheckBoxState extends State<_CheckBoxWidget> {
       ),
     );
   }
+}
+
+class MultiLevelString {
+  final String level1;
+  final List<MultiLevelString> subLevel;
+  bool isExpanded;
+
+  MultiLevelString({
+    this.level1 = "",
+    this.subLevel = const [],
+    this.isExpanded = false,
+  });
+
+  MultiLevelString copy({
+    String? level1,
+    List<MultiLevelString>? subLevel,
+    bool? isExpanded,
+  }) =>
+      MultiLevelString(
+        level1: level1 ?? this.level1,
+        subLevel: subLevel ?? this.subLevel,
+        isExpanded: isExpanded ?? this.isExpanded,
+      );
+
+  @override
+  String toString() => level1;
 }
