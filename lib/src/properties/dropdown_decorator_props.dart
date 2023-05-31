85c8bc44af0e82593558b64bd771df38b9a679fb
obemu <emanueloberholzer400@gmail.com>
2023-05-31 08:39:06 +0200

    Added a few more properties to DropDownDecoratorProps

--- lib/src/properties/dropdown_decorator_props.dart
+++ lib/src/properties/dropdown_decorator_props.dart
@@ -6,10 +6,22 @@ class DropDownDecoratorProps {
   final TextAlign? textAlign;
   final TextAlignVertical? textAlignVertical;
 
+  /// Defaults to the value of [ThemeData.splashColor] or the nearest [Theme].
+  final Color? searchSplashColor;
+
+  /// Defaults to the value of [ThemeData.highlightColor] or the nearest [Theme].
+  final Color? searchHighlightColor;
+
+  /// Defaults to the value of [ThemeData.hoverColor] or the nearest [Theme].
+  final Color? searchHoverColor;
+
   const DropDownDecoratorProps({
     this.dropdownSearchDecoration,
     this.baseStyle,
     this.textAlign,
     this.textAlignVertical,
+    this.searchSplashColor,
+    this.searchHighlightColor,
+    this.searchHoverColor,
   });
 }
