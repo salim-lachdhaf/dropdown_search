c701ffe33a5e1886d9b578d2ea03622f52457916
Anil Thapa <anil.thapa@8squarei.com>
2023-06-15 19:54:39 +0545

    fix: dropdown builder should not return widget if selected item is null

--- lib/dropdown_search.dart
+++ lib/dropdown_search.dart
@@ -384,10 +384,10 @@ class DropdownSearchState<T> extends State<DropdownSearch<T>> {
                 baseStyle: widget.dropdownDecoratorProps.baseStyle,
                 textAlign: widget.dropdownDecoratorProps.textAlign,
                 textAlignVertical: widget.dropdownDecoratorProps.textAlignVertical,
-                isEmpty: getSelectedItem == null && widget.dropdownBuilder == null,
+                isEmpty: getSelectedItem == null,
                 isFocused: isFocused,
                 decoration: _manageDropdownDecoration(state),
-                child: _defaultSelectedItemWidget(),
+                child:(getSelectedItem!=null)? _defaultSelectedItemWidget():null,
               );
             });
       },
@@ -416,10 +416,10 @@ class DropdownSearchState<T> extends State<DropdownSearch<T>> {
                 baseStyle: widget.dropdownDecoratorProps.baseStyle,
                 textAlign: widget.dropdownDecoratorProps.textAlign,
                 textAlignVertical: widget.dropdownDecoratorProps.textAlignVertical,
-                isEmpty: getSelectedItems.isEmpty && widget.dropdownBuilderMultiSelection == null,
+                isEmpty: getSelectedItems.isEmpty,
                 isFocused: isFocused,
                 decoration: _manageDropdownDecoration(state),
-                child: _defaultSelectedItemWidget(),
+                child:(getSelectedItems.isNotEmpty)? _defaultSelectedItemWidget():null,
               );
             });
       },
